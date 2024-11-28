d795eb812f1251f05366ea34a657fa7a79a08c0c
TheBaconStyle <maks.christmas2001@gmail.com>
2024-11-28 09:16:42 +0200

    feat(email test server): add dev.compose with email test server

--- packages/db/lib/index.d.ts
+++ packages/db/lib/index.d.ts
@@ -1,13 +1,8 @@
-import { Pool } from "pg";
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 export declare const connectionString: string;
 export declare function createConnectionString(user: string, password: string, host: string, port: string, dbName: string): string;
 export * as schema from "./schema";
-export declare const db: NodePgDatabase<typeof schema> & {
-    $client: Pool;
-};
+export declare const db: any;
 export type DBType = NodePgDatabase<typeof schema>;
-export declare function createDrizzleDB(...args: Parameters<typeof createConnectionString>): NodePgDatabase<typeof schema> & {
-    $client: Pool;
-};
+export declare function createDrizzleDB(...args: Parameters<typeof createConnectionString>): any;
